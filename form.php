66f82105d05e5b25fbb3351929f9936f14d1b399
lejohn <s3401071@student.rmit.edu.au>
2013-08-15 00:58:49 +1000

    Update form.php

--- form.php
+++ form.php
@@ -4,14 +4,13 @@
      "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-     <title><?php echo "Winestore" ?></title>
+     <title>Winestore</title>
 </head>
 <body>
     <p>Test Page.</p>
     <form>
     	<p>Region: 
     	    <select name="regiondrop">
-    	    <?php ?> 
     	    <option value=""></option>
          </select>
     	</p>
